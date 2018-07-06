1a849cdd9a2bdf0a6a5da804e2dfc89b5fa3c66c
LuK1337 <priv.luk@gmail.com>
2018-07-06 14:53:18 +0200

    Override AVB version in kernel cmdline
    * Fixes vintf vendor <> system mismatches, see:
      Runtime info <==> Framework Compatibility Matrix? 0, AVB version 1.0 does not match framework matrix 0.0
    
    Change-Id: Ia26a84cb38e2f03025809a3ec52587481a6d8217

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,7 @@ ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += androidboot.avb_version=0.0 androidboot.vbmeta.avb_version=0.0
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
