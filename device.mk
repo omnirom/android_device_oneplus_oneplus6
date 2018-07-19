cfef3b5c09edaf6ec9e426beaca3921405f1be80
darkobas <darkobas@gmail.com>
2018-07-19 15:07:43 +0200

    dont copy aosp volue tables
    Change-Id: I60b653ea40217826cd9a0b813735c6043bcaf902

--- device.mk
+++ device.mk
@@ -153,9 +153,7 @@ USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_configuration_generic.xml:system/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/primary_audio_policy_configuration.xml:system/etc/primary_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
