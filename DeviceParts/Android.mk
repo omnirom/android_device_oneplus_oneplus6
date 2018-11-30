c9c79810e1a1e7de02fd7a56d3dc628e30de96e4
Marko Man <darkobas@gmail.com>
2018-11-30 10:36:07 +0100

    make room for younger brother
    Change-Id: I19391e2ea51f9a58685478935763bc48e01d4890

--- DeviceParts/Android.mk
+++ DeviceParts/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),oneplus6))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),oneplus6 oneplus6t))
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
