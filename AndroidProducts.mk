bc48de76d1cb4c9472ae580ff751ad7fbb507ebb
Marko Man <darkobas@gmail.com>
2019-09-06 18:35:02 +0200

    update lunch combos
    Change-Id: I83083750bfad2d33f5c6466fd2d2e2b16286aa47

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
     $(LOCAL_DIR)/omni_oneplus6.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_oneplus6-user \
+    omni_oneplus6-userdebug \
+    omni_oneplus6-eng \
