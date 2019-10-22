881cc2606c7a654ed3e7b2efc666463ad5a5c283
micky387 <mickaelsaibi@free.fr>
2019-10-22 08:04:05 +0200

    op6/6t: Add Common extract-files
    - Sync extractors with templates
    
     * This introduces fixup scripts and kang mode and fixes
       passing of arguments longer than one word,
       such as --section "Hello world".
    
    Change-Id: I35d849aaa0be0860fed09844aadab66189619e33

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -19,65 +19,10 @@
 set -e
 
 # Required!
-DEVICE=oneplus6
-VENDOR=oneplus
+export DEVICE=oneplus6
+export DEVICE_COMMON=oneplus6
+export VENDOR=oneplus
 
-INITIAL_COPYRIGHT_YEAR=2018
+export DEVICE_BRINGUP_YEAR=2018
 
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/omni/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-# Copyright headers and guards
-write_headers "oneplus6"
-
-# The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# Qualcomm BSP blobs - we put a conditional around here
-# in case the BSP is actually being built
-printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$PRODUCTMK"
-printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$ANDROIDMK"
-
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt
-
-# Qualcomm performance blobs - conditional as well
-# in order to support Cyanogen OS builds
-cat << EOF >> "$PRODUCTMK"
-endif
-
--include vendor/extra/devices.mk
-ifneq (\$(call is-qc-perf-target),true)
-EOF
-
-cat << EOF >> "$ANDROIDMK"
-endif
-
-ifneq (\$(TARGET_HAVE_QC_PERF),true)
-EOF
-
-write_makefiles "$MY_DIR"/proprietary-files-qc-perf.txt
-
-echo "endif" >> "$PRODUCTMK"
-
-cat << EOF >> "$ANDROIDMK"
-
-endif
-
-EOF
-
-# We are done!
-write_footers
-
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles-common.sh" "$@"
