888eb4d09310a9bf47315ddd47b617e3209570bd
Marko Man <darkobas@gmail.com>
2019-11-17 17:48:58 +0100

    Revert "op6/6t: Add Common extract-files"
    This reverts commit 20bbf6c756a9d4c474603b4d94f4156e87a6fedc.
    
    Change-Id: I5e54b87eaecdc501ccbf2458a718c37c8eaefbf3

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -19,10 +19,65 @@
 set -e
 
 # Required!
-export DEVICE=oneplus6
-export DEVICE_COMMON=oneplus6
-export VENDOR=oneplus
+DEVICE=oneplus6
+VENDOR=oneplus
 
-export DEVICE_BRINGUP_YEAR=2018
+INITIAL_COPYRIGHT_YEAR=2018
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles-common.sh" "$@"
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/omni/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+# Copyright headers and guards
+write_headers "oneplus6"
+
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# Qualcomm BSP blobs - we put a conditional around here
+# in case the BSP is actually being built
+printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$PRODUCTMK"
+printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$ANDROIDMK"
+
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt
+
+# Qualcomm performance blobs - conditional as well
+# in order to support Cyanogen OS builds
+cat << EOF >> "$PRODUCTMK"
+endif
+
+-include vendor/extra/devices.mk
+ifneq (\$(call is-qc-perf-target),true)
+EOF
+
+cat << EOF >> "$ANDROIDMK"
+endif
+
+ifneq (\$(TARGET_HAVE_QC_PERF),true)
+EOF
+
+write_makefiles "$MY_DIR"/proprietary-files-qc-perf.txt
+
+echo "endif" >> "$PRODUCTMK"
+
+cat << EOF >> "$ANDROIDMK"
+
+endif
+
+EOF
+
+# We are done!
+write_footers
+
