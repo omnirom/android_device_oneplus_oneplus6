6023de3fc2294fd29e9af472df9c89c69f7d6caf
maxwen <max.weninger@gmail.com>
2020-02-04 14:54:21 +0100

    lights: build full hal instead of only the lib
    we dont need a rc file cause we are overlaying the hal in
    vendor and the rc file there is just starting it
    as before
    
    Change-Id: I1f67577eae559b8688462ff753a077b6c38b236a

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.oneplus6
+    android.hardware.light@2.0-service.oneplus6
 
 PRODUCT_PACKAGES += \
     android.hardware.light-V2.0-java \
@@ -177,6 +177,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_video.xml
 
 PRODUCT_PACKAGES += android.hardware.health@2.0-service.oneplus6
+
 #DEVICE_FRAMEWORK_MANIFEST_FILE += \
     system/libhidl/vintfdata/manifest_healthd_exclude.xml
 
