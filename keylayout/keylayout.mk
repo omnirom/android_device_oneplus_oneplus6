950ba6f59e73054347016e9d581564d78316083e
Marko Man <darkobas@gmail.com>
2020-04-29 21:29:21 +0200

    op6: change fpc longpress gesture key
    Change-Id: Ice4c82d55c09dcc325176f2dc2ede3e476e11c59

--- keylayout/keylayout.mk
+++ keylayout/keylayout.mk
@@ -1,7 +1,4 @@
 ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),oneplus6))
 PRODUCT_COPY_FILES += \
     device/oneplus/oneplus6/keylayout/gf_input.kl:system/usr/keylayout/gf_input.kl
-else
-PRODUCT_COPY_FILES += \
-    device/oneplus/oneplus6/keylayout/gf_input_op6t.kl:system/usr/keylayout/gf_input.kl
 endif
